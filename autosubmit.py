495531dfe018d6c2b38f727b26eaaca8aff7d244
YeonhoShin <43157214+JoinNova@users.noreply.github.com>
2019-02-01 09:52:28 +0900

    Update autosubmit.py

--- autosubmit.py
+++ autosubmit.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
+from time import sleep
 browser = webdriver.Chrome(executable_path=r"C:\Users\win10\AppData\Local\Programs\Python\chromedriver.exe")
-browser.get('https://www.acmicpc.net/login/?next=%2F')
+#browser.get('https://www.acmicpc.net/login/?next=%2F')
+browser.get('https://www.acmicpc.net/login?next=%2F') #사이트주소바뀜
 ID = "**************"
 PW = "**************"
 elem_login = browser.find_element_by_name("login_user_id")
@@ -11,3 +13,4 @@ browser.find_element_by_xpath('/html/body/div[3]/div[3]/div/div/form/div[4]/div[
 while True:
   browser.get('https://www.acmicpc.net/submit/10944/9280398') # 이전에 제출한 곳의 주소
   browser.find_element_by_xpath('//*[@id="submit_form"]/div[4]/div/button').click()
+  sleep(72) #72초 룰 생김 72이내 재제출 불가
